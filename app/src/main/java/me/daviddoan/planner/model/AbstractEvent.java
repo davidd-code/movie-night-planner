c324e2fd7e1a9a6b417161062b4c630ae2a1fa75
David Doan <ddoan.0411@gmail.com>
2019-04-05 16:16:36 +1100

    add constructor for abstract classes

--- app/src/main/java/me/daviddoan/planner/model/AbstractEvent.java
+++ app/src/main/java/me/daviddoan/planner/model/AbstractEvent.java
@@ -1,8 +1,21 @@
 package me.daviddoan.planner.model;
 
 import me.daviddoan.planner.model.interfaces.Event;
+import me.daviddoan.planner.model.interfaces.Movie;
 
 public class AbstractEvent implements Event {
 
     private String id, title, startDate, endDate, venue, location;
+    private Movie movie;
+
+    public AbstractEvent(String id, String title, String startDate, String endDate, String venue,
+                         String location, Movie movie) {
+        this.id = id;
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.venue = venue;
+        this.location = location;
+        this.movie = movie;
+    }
 }
--- app/src/main/java/me/daviddoan/planner/model/AbstractMovie.java
+++ app/src/main/java/me/daviddoan/planner/model/AbstractMovie.java
@@ -5,4 +5,11 @@ import me.daviddoan.planner.model.interfaces.Movie;
 public class AbstractMovie implements Movie {
 
     private String id, title, year, poster;
+
+    public AbstractMovie(String id, String title, String year, String poster) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.poster = poster;
+    }
 }
