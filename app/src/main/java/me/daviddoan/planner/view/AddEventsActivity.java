327762a0f57ad0b85b8f3fabddc7432cbbb256d0
David Doan <ddoan.0411@gmail.com>
2019-04-11 20:40:31 +1000

    add date and time picker to add events activity

--- app/src/main/java/me/daviddoan/planner/view/AddEventsActivity.java
+++ app/src/main/java/me/daviddoan/planner/view/AddEventsActivity.java
@@ -1,15 +1,65 @@
 package me.daviddoan.planner.view;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 import me.daviddoan.planner.R;
 
-public class AddEventsActivity extends AppCompatActivity {
+public class AddEventsActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+
+    private TextView startTimeTextView, startDateTextView, endTimeTextView, endDateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_events);
+
+        startTimeTextView = (TextView)findViewById(R.id.startTimeTextView);
+        startDateTextView = (TextView)findViewById(R.id.startDateTextView);
+
+        startTimeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment startTimePicker = new TimePickerFragment();
+                startTimePicker.show(getSupportFragmentManager(), "Start Time");
+            }
+        });
+
+        startDateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment startDatePicker = new DatePickerFragment();
+                startDatePicker.show(getSupportFragmentManager(), "Start Date");
+            }
+        });
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//        startDateTextView.setText(dayOfMonth + '/' + month + '/' + year);
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String dateString = DateFormat.getDateInstance().format(c.getTime());
+        startDateTextView.setText(dateString);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        startTimeTextView.setText(hourOfDay + " : " + minute);
     }
 }
+
