e957fe5f4ee7e0079972725fe56a175234d5945a
josh_ <s3690595@student.rmit.edu.au>
2019-05-25 12:37:37 +1000

    cleaned up code and refactored w/ bad solution for travel time

--- app/src/main/java/com/example/assignment_1/restapi/HttpURLConnectionThread.java
+++ app/src/main/java/com/example/assignment_1/restapi/HttpURLConnectionThread.java
@@ -28,10 +28,13 @@ public class HttpURLConnectionThread extends Thread {
 //                        HttpURLConnectionAsyncTask connect = (HttpURLConnectionAsyncTask) new HttpURLConnectionAsyncTask(currentLocation, element.getLatitude(), element.getLongitude()).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
             long travelTime = connect.getTravelTimeSeconds();
+            while(travelTime == 0){
+                travelTime = connect.getTravelTimeSeconds();
+            }
 
             long minutesUntilEvent = ChronoUnit.MINUTES.between(currentEvent.getStartDate(), currentTime);
             Log.d(TAG, "travelTime: " + travelTime);
-//            if(minutesUntilEvent - travelTime < notificationP) {
+//            if(minutesUntilEvent - travelTime < notificationP) {s
 //                displayNotification(getApplicationContext(), element.getTitle(), "is starting in " + minutesUntilEvent + " minutes. Approximately " + travelTime + " travel time.");
 //            }
 
