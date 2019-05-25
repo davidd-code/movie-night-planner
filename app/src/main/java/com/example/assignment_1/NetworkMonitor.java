1e3efdc639e0a4607d948c48319421358c2c6f5d
josh_ <s3690595@student.rmit.edu.au>
2019-05-25 12:59:53 +1000

    done network monitor waiting for the event notification to be finished

---
+++ app/src/main/java/com/example/assignment_1/NetworkMonitor.java
@@ -0,0 +1,37 @@
+package com.example.assignment_1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class NetworkMonitor extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //event notification s
+
+
+
+
+    }
+
+    public boolean isConnected(Context context) {
+        try {
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
+                return true;
+            } else {
+                Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
